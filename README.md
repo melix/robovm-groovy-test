8cd2d90b551333e26646b2b2c4849022960c51e4
Cedric Champeau <cedric.champeau@gmail.com>
2014-06-19 16:13:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Test project for Groovy on RoboVM
+=================================
+
+Prerequisites
+-------------
+
+On Linux, follow the instructions at https://github.com/robovm/robovm/wiki/Get-started-on-Linux
+
+Build
+-----
+
+```
+git clone https://github.com/melix/robovm-groovy-test.git
+cd robovm-groovy-test
+./gradlew robovm
+```
+
+
---
+++ src/main/groovy/HelloWorld.groovy
@@ -0,0 +1,24 @@
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class HelloWorld {
+    public static void main(String[] args) {
+        println 'Hello, world!'
+    }
+}
+
+// fails with
+// Exception in thread "main" java.lang.ExceptionInInitializerError
+// 	at org.codehaus.groovy.runtime.InvokerHelper.<clinit>(InvokerHelper.java)
+// 	at org.codehaus.groovy.runtime.DefaultGroovyMethods.println(DefaultGroovyMethods.java)
+// 	at HelloWorld.main(HelloWorld.groovy)
+// Caused by: java.lang.NullPointerException
+// 	at org.codehaus.groovy.runtime.metaclass.MetaClassRegistryImpl.<init>(MetaClassRegistryImpl.java)
+// 	at org.codehaus.groovy.runtime.metaclass.MetaClassRegistryImpl.<init>(MetaClassRegistryImpl.java)
+// 	at groovy.lang.GroovySystem.<clinit>(GroovySystem.java)
+// 	... 3 more
+
+
+
+// also tested this script, without @CompileStatic and implicit main, fails with the same NPE
+// println 'Hello, world!'
